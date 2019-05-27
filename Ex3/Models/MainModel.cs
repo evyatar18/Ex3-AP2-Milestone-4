6f3174db68ab33e5e0c6be12c7439da887d1f7be
danielkag2000 <45756887+danielkag2000@users.noreply.github.com>
2019-05-27 16:52:02 +0300

    Add documentation

--- Ex3/Models/MainModel.cs
+++ Ex3/Models/MainModel.cs
@@ -15,6 +15,12 @@ namespace Ex3.Models
             this.models = new ConcurrentDictionary<string, IModel>();
         }
 
+        /// <summary>
+        /// Add a ConnectionModel
+        /// </summary>
+        /// <param name="serverIp">The ip</param>
+        /// <param name="serverPort">The port</param>
+        /// <returns>The ConnectionModel with the ip and the port</returns>
         public IModel AddConnectionModel(string serverIp, int serverPort)
         {
             IModel model;
@@ -29,6 +35,13 @@ namespace Ex3.Models
             return model;
         }
 
+        /// <summary>
+        /// Add a SaveModel
+        /// </summary>
+        /// <param name="path">The path to the file</param>
+        /// <param name="serverIp">The ip</param>
+        /// <param name="serverPort">The port</param>
+        /// <returns>The ConnectionModel with the ip and the port and the file</returns>
         public IModel AddSaveModel(string path, string serverIp, int serverPort)
         {
             IModel model;
@@ -43,6 +56,11 @@ namespace Ex3.Models
             return model;
         }
 
+        /// <summary>
+        /// Add a FileModel
+        /// </summary>
+        /// <param name="path">The path to the file</param>
+        /// <returns>The FileModel with the file</returns>
         public IModel AddFileModel(string path)
         {
             IModel model;
