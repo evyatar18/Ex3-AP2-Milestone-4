ba63a598a870f4c5958d907534341b879894f850
danielkag2000 <45756887+danielkag2000@users.noreply.github.com>
2019-05-27 15:30:46 +0300

    added the function to the controller

--- Ex3/Controllers/DefaultController.cs
+++ Ex3/Controllers/DefaultController.cs
@@ -1,17 +1,54 @@
-﻿using System;
+﻿using Ex3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace Ex3.Controllers
 {
     public class DefaultController : Controller
     {
+        private static MainModel mainModel = new MainModel();
+        private IModel localModel = null;
+
         // GET: Default
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult display(string ip, int port, int timePerSec)
+        {
+            this.localModel = mainModel.AddConnectionModel(ip, port);
+            ViewBag.timePerSec = timePerSec;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult display(string path, int timePerSec)
+        {
+            this.localModel = mainModel.AddFileModel(path);
+            ViewBag.timePerSec = timePerSec;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult save(string ip, int port, int timePerSec, int seconds, string path)
+        {
+            this.localModel = mainModel.AddSaveModel(path, ip, port);
+            ViewBag.timePerSec = timePerSec;
+            ViewBag.seconds = seconds;
+            return View();
+        }
+
+        [HttpPost]
+        public FlightData GetData()
+        {
+            return localModel.GetNextFlightData();
         }
     }
 }
